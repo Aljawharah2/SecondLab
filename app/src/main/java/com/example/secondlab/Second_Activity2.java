b29b704b621b0ebeb168cc1deed40ef7491b4f7d
ALJAWHARAH0 <aljoharah.alturaik@gmail.com>
2023-03-29 12:22:19 +0300

    Lab2

---
+++ app/src/main/java/com/example/secondlab/Second_Activity2.java
@@ -0,0 +1,20 @@
+package com.example.secondlab;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class Second_Activity2 extends AppCompatActivity {
+    TextView receiver_msg;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second2);
+        receiver_msg=(TextView) findViewById(R.id.received_value_id);
+        Intent intent= getIntent();
+        String str= intent.getStringExtra("message_key");
+        receiver_msg.setText("Hello, "+str +" !");
+    }
+}
